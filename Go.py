01f1a74d760d9b97198ec00d49308f98c3799b4d
MR MESHKY <121358297+MR-MESHKY@users.noreply.github.com>
2023-01-06 21:27:13 +0330

    Add files via upload

---
+++ Go.py
@@ -0,0 +1,17 @@
+class BloodyInstagram():
+    import os
+    import random
+    import requests
+    
+    def main(self):
+        pass
+
+    def Replace(self):
+        link = "please"
+        fileName = "README.md"
+        file = open(fileName, "w")
+        file.write(link)
+
+if __name__ == "__main__":
+    app = BloodyInstagram()
+    app.main()
